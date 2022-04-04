c3b43e5d4d8f142ef4400ddfafa58ca7f6cc3a47
Jithin Mathew <jithinjosemathewoo8@gmail.com>
2022-04-04 01:39:02 -0500

    Main page UI updates

--- app/src/main/java/com/example/demoapplication/MainActivity.java
+++ app/src/main/java/com/example/demoapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.demoapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton droneControlScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        droneControlScreen = findViewById(R.id.introImage);
+        droneControlScreen.setOnClickListener(v -> {
+            Intent droneControlScreenIntent = new Intent(MainActivity.this, droneController.class);
+            startActivity(droneControlScreenIntent);
+        });
+
+
     }
 }
